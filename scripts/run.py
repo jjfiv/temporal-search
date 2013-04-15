6f96f5aebdb6c9191432580beebf226d49606aad
John Foley <john.foley.iv@gmx.com>
2013-04-14 22:20:08 -0400

    Make a wrapper for scala:run that expands args

---
+++ scripts/run.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+
+import subprocess, os, sys, time
+
+def streamCmd(cmd):
+  proc = subprocess.Popen(cmd, stdout=sys.stdout, stderr=sys.stderr, shell=True)
+  while proc.poll() == None:
+    pass
+  rc = proc.returncode
+  return rc
+
+if __name__ == '__main__':
+  relArgs = sys.argv[1:]
+  fixedArgs = []
+  for arg in relArgs:
+    if os.path.sep in arg:
+      fixedArgs += [os.path.abspath(arg)]
+    else:
+      fixedArgs += [arg]
+
+  addArgs = '"' + '|'.join(fixedArgs) + '"'
+
+  startTime = time.clock()
+  rc = streamCmd("mvn scala:run -q -DaddArgs="+addArgs)
+  endTime = time.clock()
+
+  if rc != 0:
+    print("rc: "+str(rc))
+  print("TOTAL TIME: %1.3fs" % (endTime-startTime))
+

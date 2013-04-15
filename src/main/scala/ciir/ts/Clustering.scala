71ee50a804eab55f8696993de97d182c4154df75
John Foley <john.foley.iv@gmx.com>
2013-04-14 22:40:47 -0400

    Briefly learn about junit

--- src/main/scala/ciir/ts/Clustering.scala
+++ src/main/scala/ciir/ts/Clustering.scala
@@ -108,27 +108,6 @@ object KMeans {
   }
 
   def test() = {
-    val data: Array[Tuple2[String, Array[Double]]] = Array(
-      ("chocolate", Array(9, 9, 0, 0, 0)),
-      ("vanilla", Array(0, 8, 7, 0, 0)),
-      ("strawberry", Array(4, 8, 2, 0, 0)),
-      
-      ("puppies", Array(0, 1, 4, 6, 2)),
-      ("kittens", Array(1, 0, 2, 8, 2)),
-      ("ducklings", Array(0, 0, 2, 5, 5))
-    )
-
-    val vectors = data.map(_._2)
-    val labels = data.map(_._1)
-
-    //println(vectors.map("["+_.mkString(",")+"]").mkString(","))
-    //println(labels.mkString(","))
-
-    val clusters = kmeans(2, vectors, labels)
-
-    clusters.zipWithIndex.foreach {
-      case (contents, i) => println("Cluster "+i+": "+contents.map(labels(_)).mkString(","))
-    }
   }
 }
 

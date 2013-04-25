3ac18b628ce374be255ffae76d82edd40e50ae23
John Foley <john.foley.iv@gmx.com>
2013-04-25 15:29:38 -0400

    CharacterStream: a useful character input class

--- src/main/scala/ciir/ts/IO.scala
+++ src/main/scala/ciir/ts/IO.scala
@@ -111,6 +111,99 @@ object IO {
 
 }
 
+class CharacterStream(path: String) {
+  private var fp = IO.textInputStream(path)
+  private var buf = new collection.mutable.Queue[Char]()
+
+  private def getc() = fp.read()
+  // Try to make sure the internal buffer is of a given length
+  // obviously, the real file may not cooperate
+  private def tryEnsureLength(len: Int) {
+    var idx = buf.size
+    while(idx < len) {
+      val next = getc()
+      if(next == -1) {
+        return false; // can't make it the right length
+      }
+      buf += next.toChar
+    }
+    true
+  }
+  private def growBuffer() = tryEnsureLength(buf.size*2)
+  private def fromBuf(idx: Int) = {
+    // grow exponentiallly in size as we need more
+    if(idx >= buf.size) growBuffer()
+
+    // return the character or else EOF
+    if(idx < buf.size) buf(idx) else -1
+  }
+
+  // file open/close
+  def more() = peek != -1
+  def done() = !more()
+  def close() { 
+    buf.clear()
+    fp.close()
+  }
+
+  // peek methods
+  def peek: Int = fromBuf(0)
+  def peek(count: Int): Option[String] = {
+    if(!tryEnsureLength(count)) return None
+    Some(buf.take(count).toString)
+  }
+  def peekUntil(marker: Char): Option[String] = {
+    // grow the buffer until it contains the marker we're looking for
+    while(!buf.contains(marker)) {
+      if(!growBuffer()) {
+        return None
+      }
+    }
+    return Some(buf.takeWhile(_ != marker))
+  }
+  def peekMatches(str: String): Boolean = {
+    peek(str.size) match {
+      case None => false
+      case Some(next) => next == str
+    }
+  }
+
+  // if you subsist upon peeks alone, you'll be dropping often
+  def drop(amt: Int) {
+    buf = buf.drop(amt)
+  }
+  def dropUntil(marker: Char) {
+    buf = buf.dropWhile(_ != marker)
+  }
+  def dropIncluding(marker: Char) {
+    dropUntil(marker); drop()
+  }
+  // get methods
+  def get(): Int = {
+    if(tryEnsureLength(1)) buf.dequeue() else -1
+  }
+  def get(count: Int): Option[String] = {
+    val res = peek(count)
+    drop(count)
+    res
+  }
+  def getUntil(marker: Char): Option[String] = {
+    val res = peekUntil(marker)
+    dropUntil(marker)
+  }
+  def getIncluding(marker: Char): Option[String] = {
+    getUntil(marker) match {
+      case None => None
+      case Some(str) => {
+        if(peek == -1) None
+        else {
+          Some(str + get().toChar)
+        }
+      }
+    }
+  }
+}
+
 object XMLStream {
   def simpleGetKeys(path: String, keys: Set[String]): Map[String,String] = {
     var mb = Map.newBuilder[String,String]
@@ -120,7 +213,7 @@ object XMLStream {
       xmlStream = new XMLStream(path)
       var done = false
       while(!done) {
-        xmlStream.nextTag match {
+        xmlStream.nextTag() match {
           case Some(tag) => {
             val tagName = tag.takeWhile(!_.isWhitespace)
             if(keys.contains(tagName)) {
@@ -152,75 +245,18 @@ object XMLStream {
   }
 }
 
-class XMLStream(path: String) {
-  var inputStream = IO.textInputStream(path)
-  def close() { inputStream.close() }
-  
-  def peek: Option[Char] = {
-    assert(inputStream.markSupported())
-    inputStream.mark(1)
-    val theValue = inputStream.read()
-    inputStream.reset()
-    
-    if(theValue == -1) None
-    else Some(theValue.toChar)
-  }
+class XMLStream(path: String) extends CharacterStream(path) {
+  def nextTag(): Option[String] = {
+    dropIncluding('<')
 
-  def done = peek.isEmpty
-  def skip() { inputStream.skip(1L) }
-
-  def next(): Option[Char] = {
-    val nextInt = inputStream.read()
-    if(nextInt == -1) {
+    if(done()) {
       None
     } else {
-      Some(nextInt.toChar)
+      val tag = getUntil('>');
+      drop() // the '>' character
+      tag
     }
   }
-
-  def discardUntil(marker: Char) {
-    while(true) {
-      peek match {
-        case None => { return }
-        case Some(c) => {
-          if(c == marker)
-            return
-          skip()
-        }
-      }
-    }
-  }
-  def readUntil(marker: Char) = {
-    var sb = new StringBuilder
-    var done = false
-    while(!done) {
-      peek match {
-        case None => { done = true }
-        case Some(c) => {
-          if(c == marker) {
-            done = true
-          } else {
-            sb += c
-            skip()
-          }
-        }
-      }
-    }
-    sb.result
-  }
-
-  def nextTag: Option[String] = {
-    discardUntil('<')
-    skip()
-
-    if(done) {
-      None
-    } else {
-      val tag = readUntil('>');
-      skip()
-      Some(tag)
-    }
-  }
-  def nextData: String = { readUntil('<') }
+  def nextData(): String = { getUntil('<').getOrElse("") }
 }
 

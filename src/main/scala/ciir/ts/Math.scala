709777a207d14db525b2fc135c7820f5805bc557
John Foley <john.foley.iv@gmx.com>
2013-04-25 10:25:15 -0400

    Implement JS Divergence measure
     - Thought it would fair better against artificial curves

--- src/main/scala/ciir/ts/Math.scala
+++ src/main/scala/ciir/ts/Math.scala
@@ -59,5 +59,43 @@ object Math {
 
     - math.log(sum)
   }
+
+  // http://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence
+  def JSDivergenceFlat(as: Array[Int], bs: Array[Int]): Double = {
+    // calculate the size of the input arrays so as to turn them into proper probabilities
+    val (asum, bsum) = {
+      var atotal = 0
+      var btotal = 0
+      var idx = 0
+      while(idx < as.size) {
+        atotal += as(idx)
+        btotal += bs(idx)
+        idx += 1
+      }
+      
+      (atotal.toDouble, btotal.toDouble)
+    }
+
+    // do the calculation of m, KL(a,m) and KL(b,m) in one loop for efficiency
+    var amScore = 0.0
+    var bmScore = 0.0
+    var idx = 0
+    while(idx < as.size) {
+      val ai = as(idx).toDouble / asum
+      val bi = bs(idx).toDouble / bsum
+      val mi = ai + bi / 2.0
+
+      if(ai != 0) amScore += math.log(ai/mi) * ai
+      if(bi != 0) bmScore += math.log(bi/mi) * bi
+
+      idx += 1
+    }
+    
+    (amScore + bmScore) / 2.0
+  }
+
+  def JSSimilarity(as: Array[Int], bs: Array[Int]): Double = {
+    1.0 - JSDivergenceFlat(as,bs)
+  }
 }
 
